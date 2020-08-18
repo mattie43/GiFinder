63226f88f8bded93506a12acc9b4b0ea7b4ab604
alerner11 <alyssaplerner@gmail.com>
2020-08-18 15:26:44 -0400

    switched required gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,6 +57,7 @@ class User < ActiveRecord::Base
 
     def sign_out
         self.class.current_user = false
+        puts "You have been signed out."
         welcome_screen
     end
 
@@ -91,32 +92,95 @@ def task_selection_screen
     end
 end
 
-# always have option to return to selection screen
+def return_to_selection_screen
+    puts "To return to the task selection screen, hit the Enter or Return key."
+    gets.chomp
+    self.task_selection_screen
+end
 
-# User class (instance method)
 def create_category
     puts "What is the name of the new category?"
     new_category = gets.chomp
 
     Category.create(name: new_category, user_id: self.id)
 
-    puts "Thank you, your category has been created. To return to the task selection screen, hit the Enter or Return key."
-    gets.chomp
-    self.task_selection_screen
+    puts "Thank you, your category has been created."
 
+    self.return_to_selection_screen
 end
 
 # User class (instance method)
 def view_categories
+    if self.categories.length == 0
+        puts "You have not created any categories yet."
+        puts "If you would like to create one, type \"create category\". Otherwise, hit the Enter or Return key to return to the task selection screen."
+        
+        new_category = gets.chomp
+
+        if new_category == "create category"
+            self.create_category
+        else
+            self.task_selection_screen
+        end
+
+    else
+        puts "Your categories are:"
+        self.categories.all.each do |category|
+            puts category.name
+        end
+
+        puts "To view gifs in a category, type that category's name. Otherwise, hit the Enter or Return key to return to the task selection screen."
+
+        category_choice = gets.chomp
+
+        if self.categories.find_by(name: category_choice)
+            self.view_category(category_choice)
+        elsif category_choice = ""
+            self.return_to_selection_screen
+        else 
+            puts "Sorry, that is not a valid category."
+            self.return_to_selection_screen
+        end
+    end
+
     # check if categories, if none ask if user wishes to create one
     # display list of categories, then ask to choose or return to task selection screen
 end
 
+def view_category(category_name)
+    category = self.categories.find_by(name: category_name)
+    category.gifs.each do |gif|
+        puts gif.nickname
+    end
+
+    puts "To view a gif, type its name. Otherwise, hit the Enter or Return key to return to the task selection screen."
+
+    gif_choice = gets.chomp
+
+    if self.gifs.find_by(name: gif_choice)
+        self.view_gif(gif_choice)
+    elsif gif_choice = ""
+        self.return_to_selection_screen
+    else 
+        puts "Sorry, that is not a valid gif."
+        self.return_to_selection_screen
+    end
+end
+
 # User class (instance method)
 def select_category
-    # if category doesn't exist, ask if you want to create it
-    # if not, error? and send back to selection screen
-    # display gif nicknames
-    # ask to choose gif or return to task selection screen
+    puts "Please enter the category name:"
+    category_name = gets.chomp
+
+    if self.categories.find_by(name: category_name)
+        self.view_category(category_name)
+    elsif category_name = ""
+        self.return_to_selection_screen
+    else 
+        puts "Sorry, that is not a valid category."
+        self.return_to_selection_screen
+    end
 end
+
+
 end
