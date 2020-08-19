c3ae5c57d99caaaee3f9289cfe8942f6a47d77c1
Mattie43 <ericksenm93@ymail.com>
2020-08-18 21:29:18 -0400

    finished category

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,17 +10,14 @@ class Category < ActiveRecord::Base
         # ask for nickname
         # stretch: display gif (if not just link)
         # stretch: ask if you want to share etc
-        puts "Input the name of the gif you're looking for:"
-        name = gets.chomp
+        name = prompt.ask("Enter the name of the gif you're looking for:") 
         gif = Gif.all.find_by(category: self, nickname: name)
-        
+
         if gif == nil
             puts "There is no gif with that nickname in this category."
         else
-            puts gif.link
-            puts "Would you like to share this gif? (y/n)"
-            input = gets.chomp
-            gif.share_gif if input.downcase == "y"
+            Gif.display_gif(gif.link)
+            gif.share_gif if prompt.yes?("Would you like to share this gif?") 
         end
     end
 
