4e15d0e626da4be3afd667d0e770bf449cf4b41a
alerner1 <alyssplerner@gmail.com>
2020-08-21 09:16:55 -0400

    moved view gifs method to category class

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,22 @@
 class Category < ActiveRecord::Base
     belongs_to :user, touch: true
     has_many :gifs    
+
+    def view_gifs(category_name)
+        system('clear')
+
+        chosen_category = self.categories.find_by(name: category_name)
+        
+        gif_names = chosen_category.gifs.all.map { |gif| gif.nickname } << "Return to menu"
+
+        gif_choice = TTY::Prompt.new.enum_select("Select a gif, or return to menu.", gif_names)
+
+        gif_ins = chosen_category.gifs.find_by(nickname: gif_choice)
+        if chosen_category.gifs.find_by(nickname: gif_choice)
+            # display, then delete/share/move
+            self.delete_share_move(gif_ins)
+        elsif gif_choice == "Return to menu"
+            self.task_selection_screen
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -128,7 +128,7 @@ class User < ActiveRecord::Base
 
                 case choice
                 when "view gifs"
-                    self.view_gifs(category_choice)
+                    Category.view_gifs(category_choice)
                 when "change name"
                     new_name = TTY::Prompt.new.ask("What is the new name for the category?\n")
 
@@ -144,23 +144,7 @@ class User < ActiveRecord::Base
         end
     end
 
-    def view_gifs(category_name)
-        system('clear')
 
-        chosen_category = self.categories.find_by(name: category_name)
-        
-        gif_names = chosen_category.gifs.all.map { |gif| gif.nickname } << "Return to menu"
-
-        gif_choice = TTY::Prompt.new.enum_select("Select a gif, or return to menu.", gif_names)
-
-        gif_ins = chosen_category.gifs.find_by(nickname: gif_choice)
-        if chosen_category.gifs.find_by(nickname: gif_choice)
-            # display, then delete/share/move
-            self.delete_share_move(gif_ins)
-        elsif gif_choice == "Return to menu"
-            self.task_selection_screen
-        end
-    end
 
     def delete_share_move(gif_ins)
         Gif.display_gif(gif_ins.link)
