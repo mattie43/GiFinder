8cb4cf3f20c4f38eb34e7bfc2cb4f3089a063a91
alerner1 <alyssplerner@gmail.com>
2020-08-21 09:18:40 -0400

    moved delete/share/move method to category class

--- app/models/category.rb
+++ app/models/category.rb
@@ -16,6 +16,26 @@ class Category < ActiveRecord::Base
             # display, then delete/share/move
             self.delete_share_move(gif_ins)
         elsif gif_choice == "Return to menu"
+            User.task_selection_screen
+        end
+    end
+
+    def delete_share_move(gif_ins)
+        Gif.display_gif(gif_ins.link)
+        options = %w(Delete Move\ categories Share Return\ to\ menu)
+        answer = TTY::Prompt.new.select("What would you like to do with this gif?", options)
+        case answer
+        when "Delete"
+            system 'clear'
+            print TTY::Box.warn("Gif will be deleted permanently.")
+            key_pressed = TTY::Prompt.new.keypress("Press ENTER to continue with deletion, or ESC to cancel.", keys: [:return, :escape])
+            gif_ins.delete if key_pressed == "\r"
+            self.task_selection_screen
+        when "Move categories"
+            # add move
+        when "Share"
+            gif_ins.share_gif
+        when "Return to menu"
             self.task_selection_screen
         end
     end
