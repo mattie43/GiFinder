180ca2b43b6acbed41bc69e82eeacbc65a814e7b
alerner1 <alyssplerner@gmail.com>
2020-08-21 09:15:24 -0400

    added ability to change category name

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
             self.login
         else
             self.current_user = self.find_by(username: username)
-            print TTY::Box.success("Welcome back #{username}!")
+            print TTY::Box.success("Welcome, #{username}!")
             sleep(2.0)
             self.current_user.task_selection_screen
         end
@@ -120,10 +120,24 @@ class User < ActiveRecord::Base
         else
             category_names = self.categories.all.map { |category| category.name } << "Return to menu"
 
-            category_choice = TTY::Prompt.new.enum_select("Select a category to view its gifs, or return to menu.", category_names)
+            #TODO: add deleting category once we have a system for moving gifs (can only delete once empty)
+            category_choice = TTY::Prompt.new.enum_select("Select a category to change its name or view its gifs, or return to menu.", category_names)
 
             if self.categories.find_by(name: category_choice)
-                self.view_gifs(category_choice)
+                choice = TTY::Prompt.new.select("What would you like to do with the #{category_choice} category?", %w(view\ gifs change\ name))
+
+                case choice
+                when "view gifs"
+                    self.view_gifs(category_choice)
+                when "change name"
+                    new_name = TTY::Prompt.new.ask("What is the new name for the category?\n")
+
+                    categories.find_by(name: category_choice).update(name: new_name)
+                    puts "The #{category_choice} category has been renamed to the #{new_name} category. Push Enter to return to the task selection screen."
+                    gets.chomp
+                    self.task_selection_screen
+                end
+                
             elsif category_choice == "Return to menu"
                 self.task_selection_screen
             end
